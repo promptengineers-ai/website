6dd88957dc7670e4e6085131b1f0ece5aa5a8aa8
kre8mymedia <kre8mymedia@gmail.com>
2024-07-20 15:56:23 -0500

    Will submit to PE list

--- src/app/api/contact/route.ts
+++ src/app/api/contact/route.ts
@@ -3,8 +3,9 @@ import { NextResponse } from "next/server";
 export async function POST(request: Request) {
 
   const API_TOKEN = process.env.AIRTABLE_API_KEY;
-  const URL = 'https://api.airtable.com/v0/appVmzN4ApCCGxBmC/Contacts';
+  const URL = "https://api.airtable.com/v0/app6sU4AprV9uZze6/Contacts";
   const body = await request.json();
+  delete body.Message; // If to Prompt Engineers AI contact list.
   try {
     // Forward the request to Airtable
     const response = await fetch(URL, {
@@ -19,6 +20,7 @@ export async function POST(request: Request) {
     if (!response.ok) {
       // Handles Airtable API errors
       const errorData = await response.json();
+      console.log(errorData);
       return new Response(errorData, {
         status: 500,
       });
