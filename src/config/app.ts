138a498c2c0a7627c08636df6d603ee02a7ef7ff
ryaneggz <kre8mymedia@gmail.com>
2025-01-04 12:31:01 -0600

    Updated icons

--- src/config/app.ts
+++ src/config/app.ts
@@ -5,6 +5,7 @@ import {
   FaLinkedin,
   FaInstagram,
   FaYoutube,
+  FaTwitch,
 } from "react-icons/fa";
 
 export const NODE_ENV = process.env.NEXT_PUBLIC_NODE_ENV || "production";
@@ -15,17 +16,35 @@ export const MEDIUM_RSS_URL = "https://medium.com/feed/@ryaneggz";
 // Social Config
 export const socialIcons = [
   {
+    Icon: FaYoutube,
+    tooltip: "@promptengineers",
+    key: "youtube",
+    link: "https://www.youtube.com/@promptengineersai",
+  },
+  {
+    Icon: FaTwitch,
+    tooltip: "ryaneggz",
+    key: "twitch",
+    link: "https://www.twitch.tv/ryaneggz",
+  },
+  {
+    Icon: FaInstagram,
+    tooltip: "@promptengineers.ai",
+    key: "instagram",
+    link: "https://www.instagram.com/promptengineers.ai/",
+  },
+  {
     Icon: FaGithub,
     tooltip: "promptengineers-ai",
     key: "github",
     link: "https://github.com/promptengineers-ai",
   },
-  {
-    Icon: FaGitlab,
-    tooltip: "kre8mymedia",
-    key: "gitlab",
-    link: "https://gitlab.com/kre8mymedia",
-  },
+  // {
+  //   Icon: FaGitlab,
+  //   tooltip: "kre8mymedia",
+  //   key: "gitlab",
+  //   link: "https://gitlab.com/kre8mymedia",
+  // },
   {
     Icon: FaMeetup,
     tooltip: "Plano Prompt Engineers",
@@ -38,18 +57,7 @@ export const socialIcons = [
     key: "linkedin",
     link: "https://www.linkedin.com/company/promptengineers-ai",
   },
-  {
-    Icon: FaInstagram,
-    tooltip: "@promptengineers.ai",
-    key: "instagram",
-    link: "https://www.instagram.com/promptengineers.ai/",
-  },
-  {
-    Icon: FaYoutube,
-    tooltip: "@promptengineers",
-    key: "youtube",
-    link: "https://www.youtube.com/@promptengineersai",
-  },
+  
   // {
   //   Icon: FaMedium,
   //   tooltip: "@promptengineers",
